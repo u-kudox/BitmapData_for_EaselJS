aa4a3a37c6f79a4ef936c0b0fd2825a00e9012a6
u-kudox <kudox.jp@gmail.com>
2014-01-23 21:41:02 +0900

    Setting grunt

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg : grunt.file.readJSON('package.json'),
+		uglify : {
+			bitmapdata : {
+				options : {
+					banner : grunt.file.read('src/License.js'),
+				},
+				src : ['src/BitmapData.js'],
+				dest : 'lib/bitmapdata-<%= pkg.version %>.min.js'
+			}
+		},
+		copy : {
+			bitmapdata : {
+				files : [{expand:true, cwd:'lib/', src:'bitmapdata-<%= pkg.version %>.min.js', dest:'examples/js/'}]
+			}
+		},
+		watch : {
+			bitmapdata : {
+				files : ['src/BitmapData.js', 'src/License.js'],
+				tasks : ['bitmapdata']
+			}
+		},
+		clean : {
+			bitmapdata : {
+				src : ['lib/bitmapdata-*.js', 'examples/js/bitmapdata-*.js']
+			},
+			docs : {
+				src : ['build/yuidoc', 'docs']
+			}
+		},
+		yuidoc : {
+			docs : {
+				name : 'BitmapData for EaselJS API documentation',
+				description : '<%= pkg.description %>',
+				version : '<%= pkg.version %>',
+				url : '<%= pkg.url %>',
+				options : {
+					paths : 'src',
+					outdir : 'build/yuidoc'
+				}
+			}
+		},
+		compress : {
+			docs : {
+				options : {
+					archive : 'docs/<%= pkg.name.toLowerCase() %>-<%= pkg.version %>-docs.zip'
+				},
+				files : [{expand:true, src:'**', cwd:'build/yuidoc'}]
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-yuidoc');
+	grunt.loadNpmTasks('grunt-contrib-compress');
+
+	grunt.registerTask('docs', ['clean:docs', 'yuidoc:docs', 'compress:docs']);
+	grunt.registerTask('bitmapdata', ['clean:bitmapdata', 'uglify:bitmapdata', 'copy:bitmapdata']);
+};
---
+++ src/License.js
@@ -0,0 +1,9 @@
+/*
+BitmapData for EaselJS
+Version: 1.00
+Author: kudox
+http://kudox.jp/
+http://twitter.com/u_kudox
+Licensed under the MIT License
+Copyright (c) 2013 kudox.jp
+*/
